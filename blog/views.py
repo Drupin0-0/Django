af9fac66a7a047e59a9e85cc3b6c42894a97f3cf
Drupin <linseduardo2009@gmail.com>
2025-05-05 19:19:29 -0300

    urls

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,24 @@ from django.shortcuts import render
 
 def index(request):  # Renomeado de 'blog' para 'index'
     print('index')
+    
+    context =   {
+            'text': 'estamos na blog',
+            'title': 'esse e o site do '
+        }
     return render(
         request,
-        'blog/index.html'
+        'blog/index.html',
+        context
     )
 
 def exemplo(request):
     print('exemplo')
+    context =   {
+            'text': 'estamos na home'
+        }
     return render(
         request,
-        'blog/exemplo.html'
+        'blog/exemplo.html',
+        context
     )
--- home/views.py
+++ home/views.py
@@ -3,8 +3,11 @@ from django.shortcuts import render
 
 def home(request):
     print('home')
-
+    context =   {
+            'text': 'estamos na home'
+        }
     return render(
         request,
-        'home/index.html'
+        'home/index.html',
+        context
     )
